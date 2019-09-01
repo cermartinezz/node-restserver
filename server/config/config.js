bc8f35a02c999094e7ed943860b66a7cc35a3b44
CesarMartinez <gottyrion93>
2019-09-01 11:20:45 -0600

    Restserver -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ process.env.PORT = process.env.PORT || 3000
 // ===================
 //  ENTORNO
 // ===================
-process.env.NODE_ENV = process.env_NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 // ===================
